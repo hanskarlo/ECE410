e2d18621891d36a3c1337b4e47926710348378de
Deklen Crocker <ncrocker@ualberta.ca>
2021-10-13 18:39:19 -0600

    Ajusting the testbench to correct the simulations

--- Lab1/test/full_adder_2bit_tb.vhd
+++ Lab1/test/full_adder_2bit_tb.vhd
@@ -38,32 +38,28 @@ ARCHITECTURE Behavioral OF full_adder_2bit_tb IS
 
   COMPONENT full_adder_2bit IS
     PORT (
-      A     : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-      B     : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
-      c_in  : IN STD_LOGIC;
-      sum   : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
-      c_out : OUT STD_LOGIC);
+      A              : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+      B              : IN STD_LOGIC_VECTOR (1 DOWNTO 0);
+      c_in           : IN STD_LOGIC;
+      sum            : OUT STD_LOGIC_VECTOR (1 DOWNTO 0);
+      c_out          : OUT STD_LOGIC;
+      compare_result : OUT STD_LOGIC_VECTOR(2 DOWNTO 0));
   END COMPONENT;
 
-  COMPONENT full_adder_1bit IS
-    PORT (
-      a     : IN STD_LOGIC;
-      b     : IN STD_LOGIC;
-      c_in  : IN STD_LOGIC;
-      sum   : OUT STD_LOGIC;
-      c_out : OUT STD_LOGIC);
-  END COMPONENT;
-  SIGNAL A, B        : STD_LOGIC_VECTOR(1 DOWNTO 0);
-  SIGNAL c_in, c_out : STD_LOGIC;
-  SIGNAL sum         : STD_LOGIC_VECTOR(1 DOWNTO 0);
+  SIGNAL A, B           : STD_LOGIC_VECTOR(1 DOWNTO 0);
+  SIGNAL c_in, c_out    : STD_LOGIC;
+  SIGNAL sum            : STD_LOGIC_VECTOR(1 DOWNTO 0);
+  SIGNAL compare_result : STD_LOGIC_VECTOR(2 DOWNTO 0);
+
 BEGIN
   Full_adder : full_adder_2bit PORT MAP(
-    A     => A,
-    B     => B,
-    c_in  => c_in,
-    sum   => Sum,
-    c_out => c_out);
-
+    A              => A,
+    B              => B,
+    c_in           => c_in,
+    sum            => Sum,
+    c_out          => c_out,
+    compare_result => compare_result
+  );
   PROCESS
     CONSTANT time_period : TIME := 20 ns;
 
@@ -80,7 +76,7 @@ BEGIN
     B    <= "00";
     c_in <= '0';
     WAIT FOR time_period;
-    ASSERT ((sum = "00") AND (c_out = '0')) --If for A=00 AND B=00, IF S!=00 AND C!=0, then there is an error
+    ASSERT ((sum = "00") AND (c_out = '0') AND (compare_result = "010")) --If for A=00 AND B=00, IF S!=00 AND C!=0, then there is an error
     REPORT "Error for input A_in=00 and B_in=00" SEVERITY error;
     ----------------
     ----------------
@@ -88,15 +84,15 @@ BEGIN
     B    <= "01";
     c_in <= '0';
     WAIT FOR time_period;
-    ASSERT ((sum = "11") AND (c_out = '0')) --If for A=00 AND B=00, IF S!=00 AND C!=0, then there is an error
-    REPORT "Error for input A_in=00 and B_in=00" SEVERITY error;
+    ASSERT ((sum = "11") AND (c_out = '0') AND (compare_result = "100")) --If for A=00 AND B=00, IF S!=00 AND C!=0, then there is an error
+    REPORT "Error for input A_in=00 and B_in=00 " SEVERITY error;
     ----------------
     ----------------
     A    <= "01";
     B    <= "11";
     c_in <= '0';
     WAIT FOR time_period;
-    ASSERT ((sum = "00") AND (c_out = '1')) --If for A=00 AND B=00, IF S!=00 AND C!=0, then there is an error
+    ASSERT ((sum = "00") AND (c_out = '1') AND (compare_result = "001")) --If for A=00 AND B=00, IF S!=00 AND C!=0, then there is an error
     REPORT "Error for input A_in=00 and B_in=00" SEVERITY error;
     ----------------
     ----------------
@@ -104,7 +100,7 @@ BEGIN
     B    <= "01";
     c_in <= '1';
     WAIT FOR time_period;
-    ASSERT ((sum = "11") AND (c_out = '0')) --If for A=00 AND B=00, IF S!=00 AND C!=0, then there is an error
+    ASSERT ((sum = "11") AND (c_out = '0') AND (compare_result = "010")) --If for A=00 AND B=00, IF S!=00 AND C!=0, then there is an error
     REPORT "Error for input A_in=00 and B_in=00" SEVERITY error;
     ----------------
     finish;
